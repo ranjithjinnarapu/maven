93e3883d92d2925c042dc7f1cb3f4bbbf70796c2
ranjithkumar <ranjithjinnarapu@gmail.com>
2021-05-25 15:06:26 +0530

    third commit

---
+++ src/main/java/com/icictech/App1.java
@@ -0,0 +1,13 @@
+package com.icictech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
